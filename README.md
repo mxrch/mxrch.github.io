3c08bfc1575b151fc075bcba551c4962202c183e
Mxrshy <mxrshy@mxrshy.com>
2018-10-02 10:50:25 +0200

    Mise en prod de l'homepage Google

--- README.md
+++
@@ -1,4 +0,0 @@
-# bettermotherfuckingwebsite
-A simple better mother fucking website.
-
-The live version : https://mxrch.github.io/index.html
--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-popup = function() {
-  if(window.confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')) {
-    document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-  }
-}
-
-setTimeout(popup, 10000);
