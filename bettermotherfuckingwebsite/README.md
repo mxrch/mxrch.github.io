297a4f859f1e6228096995e0634ec255e3c1daba
Mxrshy <mxrshy@mxrshy.com>
2018-10-07 22:41:03 +0200

    Ajout des projets précédents en version live

---
+++ bettermotherfuckingwebsite/README.md
@@ -0,0 +1,4 @@
+# bettermotherfuckingwebsite
+A simple better mother fucking website.
+
+The live version : https://mxrch.github.io/index.html
---
+++ bettermotherfuckingwebsite/scripts/main.js
@@ -0,0 +1,7 @@
+popup = function() {
+  if(window.confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')) {
+    document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  }
+}
+
+setTimeout(popup, 10000);
